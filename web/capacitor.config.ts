565c85026971da6862f3e9539805a95b27a7ffc1
Ali Khudher <aliomer001122@gmail.com>
2026-07-17 20:54:14 +1000

    Add Capacitor iOS setup

---
+++ web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aliomer.lifevault',
+  appName: 'LifeVault',
+  webDir: 'dist'
+};
+
+export default config;
